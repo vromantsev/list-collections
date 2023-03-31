b8445c79dcc2f1ff6468c7e2159950f34b3d884c
vromantsev <vladyslav.romantsev@gmail.com>
2023-03-31 12:45:49 +0300

    added homework 12

---
+++ common/src/main/java/ua/hillel/List.java
@@ -0,0 +1,29 @@
+package ua.hillel;
+
+public interface List<T> {
+
+    void add(T element);
+
+    void add(int index, T element);
+
+    T get(int index);
+
+    T get(T element);
+
+    T getFirst();
+
+    T getLast();
+
+    void set(int index, T element);
+
+    boolean remove(int index);
+
+    boolean contains(T element);
+
+    boolean isEmpty();
+
+    int size();
+
+    void clear();
+
+}
---
+++ linkedlist/src/main/java/ua/hillel/LinkedList.java
@@ -0,0 +1,69 @@
+package ua.hillel;
+
+/**
+ * Implement doubly linked list that is based on {@link Node<T>} class (you should implement it as well).
+ * This is a simplified version of {@link java.util.LinkedList}.
+ */
+public class LinkedList<T> implements List<T> {
+
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(T element) {
+        return null;
+    }
+
+    @Override
+    public T getFirst() {
+        return null;
+    }
+
+    @Override
+    public T getLast() {
+        return null;
+    }
+
+    @Override
+    public void set(int index, T element) {
+
+    }
+
+    @Override
+    public boolean remove(int index) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
