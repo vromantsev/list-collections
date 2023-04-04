c763558b42a696de3bf2db1a003171c6ad859b67
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-04 14:42:37 +0300

    arraylist

--- arraylist/src/main/java/ua/hillel/ArrayList.java
+++ arraylist/src/main/java/ua/hillel/ArrayList.java
@@ -1,68 +1,127 @@
 package ua.hillel;
 
+import java.util.NoSuchElementException;
+
 /**
  * Implement a list that is based on array. This resizable data structure
  * is based on an array and is a simplified version of {@link java.util.ArrayList}.
  */
 public class ArrayList<T> implements List<T> {
 
+    private T[] elements;
+    private int size;
+
+    public ArrayList() {
+        elements = (T[]) new Object[10];
+        size = 0;
+    }
+
     @Override
     public void add(T element) {
-
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size] = element;
+        size++;
     }
 
     @Override
     public void add(int index, T element) {
-
+        if (size == elements.length) {
+            resize();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            elements[i + 1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return elements[index];
     }
 
     @Override
     public T get(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return elements[i];
+            }
+        }
         return null;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return elements[0];
     }
 
     @Override
     public T getLast() {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return elements[size - 1];
     }
 
     @Override
     public void set(int index, T element) {
-
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        elements[index] = element;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        if (index < 0 || index >= size) {
+            return false;
+        }
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null;
+        size--;
+        return true;
     }
 
     @Override
     public boolean contains(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
-
+        elements = (T[]) new Object[10];
+        size = 0;
     }
+
+    private void resize() {
+        T[] newElements = (T[]) new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newElements, 0, size);
+        elements = newElements;
+    }
+
 }
---
+++ arraylist/src/main/java/ua/hillel/Demo.java
@@ -0,0 +1,4 @@
+package ua.hillel;
+
+public class Demo {
+}
