e2fdf22f99307a490ca1a54a297e4d91d70ad349
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-04 15:01:00 +0300

    arraylist

--- arraylist/src/main/java/ua/hillel/Demo.java
+++ arraylist/src/main/java/ua/hillel/Demo.java
@@ -1,4 +1,22 @@
 package ua.hillel;
 
 public class Demo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("apple");
+        list.add("banana");
+        list.add("orange");
+
+        System.out.println("Size of list: " + list.size());
+        System.out.println("First element: " + list.getFirst());
+        System.out.println("Last element: " + list.getLast());
+        System.out.println("Element at index 1: " + list.get(1));
+        System.out.println("Removing element at index 1: " + list.remove(1));
+        System.out.println("List contains 'banana': " + list.contains("banana"));
+
+        list.clear();
+
+        System.out.println("List is empty: " + list.isEmpty());
+    }
 }
--- linkedlist/src/main/java/ua/hillel/LinkedList.java
+++ linkedlist/src/main/java/ua/hillel/LinkedList.java
@@ -1,5 +1,7 @@
 package ua.hillel;
 
+import java.util.NoSuchElementException;
+
 /**
  * Implement doubly linked list that is based on {@link Node<T>} class (you should implement it as well).
  * This is a simplified version of {@link java.util.LinkedList}.
@@ -7,35 +9,136 @@ package ua.hillel;
 public class LinkedList<T> implements List<T> {
 
 
-    @Override
-    public void add(T element) {
+    public class Node<T> {
+        private T data;
+        private Node<T> prev;
+        private Node<T> next;
 
+        public Node(T data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
     }
 
-    @Override
-    public void add(int index, T element) {
 
-    }
 
-    @Override
-    public T get(int index) {
-        return null;
-    }
+        private Node<T> head;
+        private Node<T> tail;
+        private int size;
 
-    @Override
-    public T get(T element) {
-        return null;
-    }
+        public LinkedList() {
+            head = null;
+            tail = null;
+            size = 0;
+        }
 
-    @Override
-    public T getFirst() {
-        return null;
-    }
+        @Override
+        public void add(T element) {
+            Node<T> newNode = new Node<>(element);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.setNext(newNode);
+                newNode.setPrev(tail);
+                tail = newNode;
+            }
+            size++;
+        }
 
-    @Override
-    public T getLast() {
-        return null;
-    }
+        @Override
+        public void add(int index, T element) {
+            if (index < 0 || index > size) {
+                throw new IndexOutOfBoundsException();
+            }
+            Node<T> newNode = new Node<>(element);
+            if (index == 0) {
+                newNode.setNext(head);
+                head.setPrev(newNode);
+                head = newNode;
+            } else if (index == size) {
+                tail.setNext(newNode);
+                newNode.setPrev(tail);
+                tail = newNode;
+            } else {
+                Node<T> current = head;
+                for (int i = 0; i < index; i++) {
+                    current = current.getNext();
+                }
+                Node<T> prev = current.getPrev();
+                prev.setNext(newNode);
+                newNode.setPrev(prev);
+                newNode.setNext(current);
+                current.setPrev(newNode);
+            }
+            size++;
+        }
+
+        @Override
+        public T get(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            Node<T> current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.getNext();
+            }
+            return current.getData();
+        }
+
+        @Override
+        public T get(T element) {
+            Node<T> current = head;
+            while (current != null) {
+                if (current.getData().equals(element)) {
+                    return current.getData();
+                }
+                current = current.getNext();
+            }
+            return null;
+        }
+
+        @Override
+        public T getFirst() {
+            if (head == null) {
+                throw new NoSuchElementException();
+            }
+            return head.getData();
+        }
+
+        @Override
+        public T getLast() {
+            if (tail == null) {
+                throw new NoSuchElementException();
+            }
+            return tail.getData();
+        }
+
 
     @Override
     public void set(int index, T element) {
@@ -64,6 +167,8 @@ public class LinkedList<T> implements List<T> {
 
     @Override
     public void clear() {
+            
+
 
     }
 }
