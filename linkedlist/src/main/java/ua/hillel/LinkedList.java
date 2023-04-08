4f5ad35342111d85a7123259bcd04d49a82b1642
dmitriopria <dimk824@gmail.com>
2023-04-08 15:14:31 -0400

    Initial commit for LinkedList class.

--- linkedlist/src/main/java/ua/hillel/LinkedList.java
+++ linkedlist/src/main/java/ua/hillel/LinkedList.java
@@ -1,69 +1,154 @@
 package ua.hillel;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 /**
  * Implement doubly linked list that is based on {@link Node<T>} class (you should implement it as well).
  * This is a simplified version of {@link java.util.LinkedList}.
  */
 public class LinkedList<T> implements List<T> {
+    private static class Node<T> {
+        T element;
+        Node<T> next;
 
+        public Node(T element) {
+            this.element = element;
+            this.next = null;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+
+    private Node<T> getNode(int index) {
+        Node<T> curr = first;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr;
+    }
 
     @Override
     public void add(T element) {
-
+        Node<T> newNode = new Node<>(element);
+        if (first == null) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(int index, T element) {
-
+        Objects.checkIndex(index, size + 1);
+        Node<T> newNode = new Node<>(element);
+        if (first == null) {
+            first = last = newNode;
+        } else if (index == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else if (index == size) {
+            last.next = newNode;
+            last = newNode;
+        } else {
+            Node<T> previousIndex = getNode(index - 1);
+            newNode.next = previousIndex.next;
+            previousIndex.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        Node<T> node = getNode(index);
+        return (T) node;
     }
 
     @Override
     public T get(T element) {
+        Node<T> current = first;
+        for (int i = 0; i < size; i++) {
+            if (current.element.equals(element)) {
+                return current.element;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return get(0);
     }
 
     @Override
     public T getLast() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return get(size - 1);
     }
 
     @Override
     public void set(int index, T element) {
-
+        Objects.checkIndex(index, size);
+        Node<T> node = getNode(index);
+        node.element = element;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        if (index < 0 || index > size) {
+            return false;
+        }
+        if (index == 0) {
+            first = first.next;
+            if (first == null) {
+                last = null;
+            }
+        } else {
+            Node<T> previous = getNode(index - 1);
+            previous.next = previous.next.next;
+            if (index == size - 1) {
+                last = previous;
+            }
+        }
+        size--;
+        return true;
     }
 
     @Override
     public boolean contains(T element) {
+        Node<T> current = first;
+        for (int i = 0; i < size; i++) {
+            if (current.element.equals(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
-
+        first = last = null;
+        size = 0;
     }
 }
