9decad3078185bf95290340e4f2996841ce20fcf
VeronikaPl <pletnovaroni@gmail.com>
2023-04-15 16:44:29 +0300

    ArrayList hw #15 done

--- arraylist/src/main/java/ua/hillel/ArrayList.java
+++ arraylist/src/main/java/ua/hillel/ArrayList.java
@@ -1,68 +1,113 @@
 package ua.hillel;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 /**
  * Implement a list that is based on array. This resizable data structure
  * is based on an array and is a simplified version of {@link java.util.ArrayList}.
  */
 public class ArrayList<T> implements List<T> {
+    private Object[] elements;
+    private int size;
+
+    private void createNewArray() {
+        if (elements.length == size) {
+            Object[] newArray = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = newArray;
+        }
+    }
 
     @Override
     public void add(T element) {
-
+        createNewArray();
+        elements[size] = element;
+        size++;
     }
 
     @Override
     public void add(int index, T element) {
-
+        Objects.checkIndex(index, size);
+        createNewArray();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        return (T) elements[index];
     }
 
     @Override
     public T get(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return element;
+            }
+        }
         return null;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return get(0);
     }
 
     @Override
     public T getLast() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return get(size - 1);
     }
 
     @Override
     public void set(int index, T element) {
-
+        Objects.checkIndex(index, size);
+        elements[index] = element;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        if (index < 0 || index > size - 1) {
+            return false;
+        }
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        return true;
     }
 
     @Override
     public boolean contains(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 }
