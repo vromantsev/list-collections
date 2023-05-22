302ae22f9d92e810cc63fc1bd7a98cb061c3bc22
Anastasiia <lavrentjeva97@gmail.com>
2023-05-22 16:48:46 +0200

    Added changes to ArrayList and LinkedList

--- arraylist/src/main/java/ua/hillel/ArrayList.java
+++ arraylist/src/main/java/ua/hillel/ArrayList.java
@@ -5,64 +5,117 @@ package ua.hillel;
  * is based on an array and is a simplified version of {@link java.util.ArrayList}.
  */
 public class ArrayList<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+    public ArrayList(int initialCapacity) {
+        elements = new Object[initialCapacity];
+        size = 0;
+    }
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
 
     @Override
     public void add(T element) {
-
+        ensureCapacity(size + 1);
+        elements[size++] = element;
     }
 
     @Override
     public void add(int index, T element) {
-
+        if (index < 0 || size < index) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) elements[index];
     }
 
     @Override
     public T get(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements.equals(elements[i])) {
+                return (T) elements[i];
+            }
+        }
         return null;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+        return (T) elements[0];
     }
 
     @Override
     public T getLast() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+        return (T) elements[size -1];
     }
 
     @Override
     public void set(int index, T element) {
-
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsExceprion;
+        }
+        elements[index] = element;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        if (index < 0 || index >= size){
+            return false;
+        }
+        System.arraycopy (elements, index + 1, elements, index, size - index - 1);
+        size--;
+        elements[size] = null;
+        return true;
     }
 
     @Override
     public boolean contains(T element) {
-        return false;
+        return get(element) != null;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
     }
 }
--- linkedlist/src/main/java/ua/hillel/LinkedList.java
+++ linkedlist/src/main/java/ua/hillel/LinkedList.java
@@ -5,65 +5,183 @@ package ua.hillel;
  * This is a simplified version of {@link java.util.LinkedList}.
  */
 public class LinkedList<T> implements List<T> {
-
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+    private static class Node<T> {
+        private T data;
+        private Node<T> prev;
+        private Node<T> next;
+        public Node(T data) {
+            this.data = data;
+        }
+    }
 
     @Override
     public void add(T element) {
-
+        Node<T> newNode = new Node<> (element);
+        if (isEmpty ()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(int index, T element) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(element);
+        } else if (index == size) {
+            addLast(element);
+        } else {
+            Node<T> newNode = new Node<> (element);
+            Node<T> current = getNode(index);
+            newNode.prev = current.prev;
+            newNode.next = current;
+            current.prev.next = newNode;
+            current.prev = newNode;
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException;
+        }
+        Node<T> node = getNode(index);
+        return node.data;
     }
 
     @Override
     public T get(T element) {
+        Node<T> current = head;
+        while (current != null) {
+            if (current.data.equals(element)) {
+                return current.data;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        if (isEmpty ()){
+            return null;
+        }
+        return head.data;
     }
 
     @Override
     public T getLast() {
-        return null;
+        if (isEmpty ()) {
+            return null;
+        }
+        return tail.data;
     }
 
     @Override
     public void set(int index, T element) {
-
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> node = getNode(index);
+        node.data = element;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        if (index <0 || index >= size) {
+            return false;
+        }
+        if (index == 0) {
+            removeFirst();
+        } else if (index == size - 1) {
+            removeLast();
+        } else {
+            Node<T> current = getNode(index);
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+            size--;
+        }
+        return true;
     }
 
     @Override
     public boolean contains(T element) {
-        return false;
+        return get(element) != null;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    private void addFirst(T element) {
+        Node<T> newNode = new Node<> (element);
+        if (isEmpty ()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+    private void addLast(T element) {
+        add(element);
+    }
+    private void removeFirst() {
+        if (isEmpty ()) {
+            return;
+        }
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+    }
+    private void removeLast() {
+        if (isEmpty ()) {
+            return;
+        }
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else  {
+            tail = tail.next;
+            tail.prev = null;
+        }
+        size--;
+    }
+    private Node<T> getNode(int index) {
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
     }
 }
